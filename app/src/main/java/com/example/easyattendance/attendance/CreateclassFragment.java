a99845e65515430c6c2ac0cb1777e6e1d9df659d
Shikhar basnet <Shikhar basnet>
2016-12-09 22:20:14 +0545

    Class added and shown

--- app/src/main/java/com/example/easyattendance/attendance/CreateclassFragment.java
+++ app/src/main/java/com/example/easyattendance/attendance/CreateclassFragment.java
@@ -3,15 +3,20 @@ package com.example.easyattendance.attendance;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 
@@ -19,6 +24,7 @@ import android.widget.Toast;
  * A simple {@link Fragment} subclass.
  */
 public class CreateclassFragment extends Fragment implements View.OnClickListener {
+    View view;
     public CreateclassFragment() {
         // Required empty public constructor
     }
@@ -29,6 +35,20 @@ public class CreateclassFragment extends Fragment implements View.OnClickListene
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_createclass, container, false);
+        DatabaseHelper mDbHelper = new DatabaseHelper(getContext());
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM classroom",null);
+        try{
+            while (cursor.moveToNext()) {
+                LinearLayout layout = (LinearLayout) view.findViewById(R.id.linearlayouttest);
+                Button button = new Button(getActivity()); // needs activity context
+                button.setText(cursor.getString( cursor.getColumnIndex("class_name") ));
+                layout.addView(button);
+            }
+        }finally {
+
+            cursor.close();
+        }
         Button createClass = (Button)view.findViewById(R.id.button_createclass);
         createClass.setOnClickListener(this);
         return view;
@@ -47,6 +67,7 @@ public class CreateclassFragment extends Fragment implements View.OnClickListene
                         .setCancelable(false)
                         .setPositiveButton("Save", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialogBox, int id) {
+//                                cursor.close();
                                 final String classname = classnameText.getText().toString();
                                 final String classyear = classyearText.getText().toString();
                                 if (classname.length() == 0 && classyear.length() == 0){
@@ -62,6 +83,28 @@ public class CreateclassFragment extends Fragment implements View.OnClickListene
                                             "Class name field is empty", Toast.LENGTH_SHORT).show();
                                 }
                                 else{
+                                    DatabaseHelper mDbHelper = new DatabaseHelper(getContext());
+                                    SQLiteDatabase db = mDbHelper.getWritableDatabase();
+                                    Cursor cursor = db.rawQuery("SELECT * FROM classroom WHERE class_name = ?",new String[] {classname + ""});
+                                    try{
+                                        if(cursor.getCount() > 0) {
+                                            Toast.makeText(getActivity().getApplicationContext(),
+                                                    "Class already exists", Toast.LENGTH_SHORT).show();
+                                        }
+                                        else{
+                                            ContentValues values = new ContentValues();
+                                            values.put("class_name", classname);
+                                            values.put("class_year", classyear);
+                                            db.insert("classroom", null, values);
+                                            db.close(); // Closing database connection
+                                            Toast.makeText(getActivity().getApplicationContext(),
+                                                    "Class room has been created", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }finally {
+
+                                        cursor.close();
+                                    }
+
 
                                 }
                             }
--- app/src/main/java/com/example/easyattendance/attendance/DatabaseHelper.java
+++ app/src/main/java/com/example/easyattendance/attendance/DatabaseHelper.java
@@ -12,12 +12,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "attendance.db";
     private static final String CREATE_TABLE_CLASSROOM = "CREATE TABLE "
             + "classroom" + "(" + "id" + " INTEGER PRIMARY KEY AUTOINCREMENT," + "class_name"
-            + " TEXT,"+ "class_year"+"TEXT,"+"student_details"+"TEXT"+")";
+            + " TEXT," + "class_year"+" TEXT," + "student_details" + " TEXT"+")";
     private static final String CREATE_TABLE_ATTENDANCE = "CREATE TABLE "
             + "attendance" + "(" + "id" + " INTEGER PRIMARY KEY AUTOINCREMENT," + "class_id"
-            + " TEXT,"+ "year"+"TEXT,"+"month"+"TEXT,"+"day"+"TEXT,"+"attendance"+"TEXT"+")";
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DATABASE_NAME, null, 1);
+            + " TEXT," + "year" + " TEXT," + "month" + " TEXT," + "day" + " TEXT," + "attendance" + " TEXT" + ")";
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 3);
     }
 
     @Override
@@ -31,5 +31,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + "classroom");
         db.execSQL("DROP TABLE IF EXISTS " + "attendance");
+        db.execSQL(CREATE_TABLE_CLASSROOM);
+        db.execSQL(CREATE_TABLE_ATTENDANCE);
+
     }
 }
